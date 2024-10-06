32824646104c91956b4ef41301c300de404d8d76
Chris Anders <filmee24@gmail.com>
2024-10-07 00:12:02 +0200

    feat: Add Pixel Control

---
+++ src/Nonogram/Controls/Pixel.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+
+namespace Nonogram.Controls;
+
+public class Pixel : TemplatedControl
+{
+    public static readonly StyledProperty<bool> IsActivatedProperty = AvaloniaProperty.Register<Pixel, bool>(nameof(IsActivated));
+
+    public bool IsActivated
+    {
+        get => GetValue(IsActivatedProperty);
+        set => SetValue(IsActivatedProperty, value);
+    }
+
+    protected override void OnPointerReleased(PointerReleasedEventArgs e)
+    {
+        base.OnPointerReleased(e);
+
+        IsActivated = !IsActivated;
+    }
+}
