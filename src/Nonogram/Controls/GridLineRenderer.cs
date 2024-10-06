0564616ef25257a65dfb59210d2bbf1d12fe46fb
Chris Anders <filmee24@gmail.com>
2024-10-06 20:34:59 +0200

    feat: Add Custom GridLine Renderer

---
+++ src/Nonogram/Controls/GridLineRenderer.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Nonogram.Controls;
+
+public class GridLineRenderer : Control
+{
+    public static readonly StyledProperty<IBrush> LineColorProperty =
+        AvaloniaProperty.Register<GridLineRenderer, IBrush>(nameof(LineColor), Brushes.Black);
+
+    public IBrush LineColor
+    {
+        get => GetValue(LineColorProperty);
+        set => SetValue(LineColorProperty, value);
+    }
+
+    public override void Render(DrawingContext context)
+    {
+        base.Render(context);
+
+        if (Parent is Grid parentGrid)
+        {
+            var width = Bounds.Width;
+            var height = Bounds.Height;
+
+            var columnCount = parentGrid.ColumnDefinitions.Count;
+            var rowCount = parentGrid.RowDefinitions.Count;
+
+            for (var i = 0; i <= columnCount; i++)
+            {
+                var x = i * width / columnCount;
+                context.DrawLine(new Pen(LineColor), new Point(x, 0), new Point(x, height));
+            }
+
+            for (var j = 0; j <= rowCount; j++)
+            {
+                var y = j * height / rowCount;
+                context.DrawLine(new Pen(LineColor), new Point(0, y), new Point(width, y));
+            }
+        }
+    }
+}
